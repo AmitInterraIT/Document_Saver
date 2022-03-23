e1ef64f6888cd1488dca3cd50d66ef5d4e253312
tekina18 <AniketS@interrait.com>
2022-03-23 16:24:34 +0530

    workedt on table view

--- Controllers/ProjectDetailsController.cs
+++ Controllers/ProjectDetailsController.cs
@@ -27,5 +27,83 @@ namespace Document_Saver.Controllers
             IEnumerable<ProjectDetails> objProjectList = _DB.ProjectDetails;
             return View(objProjectList);
         }
+
+        public IActionResult Create(ProjectDetails obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _DB.ProjectDetails.Add(obj);
+                _DB.SaveChanges();
+                TempData["success"] = "Data Created Successfully";
+                return RedirectToAction("Table");
+            }
+            return View(obj);
+        }
+        //GET
+        public IActionResult Edit(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var ProjectDetailsFromDb = _DB.ProjectDetails.Find(Id);
+
+            if (ProjectDetailsFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProjectDetailsFromDb);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ProjectDetails obj)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _DB.ProjectDetails.Update(obj);
+                _DB.SaveChanges();
+                TempData["success"] = "Data Updated Successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var ProjectDetailsFromDb = _DB.ProjectDetails.Find(Id);
+            /*var categoryFromDbFirst = _DB.categories.FirstOrDefault(u => u.Id == Id);
+            var categoryFromDbsingle = _DB.categories.SingleOrDefault(u => u.Id == Id);*/
+            if (ProjectDetailsFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProjectDetailsFromDb);
+        }
+        //post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? Id)
+        {
+            var obj = _DB.ProjectDetails.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _DB.ProjectDetails.Remove(obj);
+            _DB.SaveChanges();
+            TempData["success"] = "Data Deleted Successfully";
+            return RedirectToAction("Table");
+
+
+
+        }
+
     }
 }
